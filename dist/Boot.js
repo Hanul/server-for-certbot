a66367da754d847617b47ab5823620f253cf4c48
Hanul <hanul@hanul.me>
2021-11-22 16:32:48 +0900

    https 지원

--- dist/Boot.js
+++ dist/Boot.js
@@ -8,14 +8,33 @@ const http_proxy_1 = __importDefault(require("http-proxy"));
 const skyserver_1 = require("skyserver");
 const Config_1 = __importDefault(require("./Config"));
 async function boot() {
-    http_proxy_1.default.createServer({
+    const wssProxy = http_proxy_1.default.createServer({
         target: "ws://localhost:9090",
         ws: true,
         ssl: {
             key: fs_1.default.readFileSync(Config_1.default.key, "utf8"),
             cert: fs_1.default.readFileSync(Config_1.default.cert, "utf8")
         }
-    }).listen(9091);
+    });
+    wssProxy.listen(9091);
+    wssProxy.on("error", function (err, req, res) {
+        console.log(err);
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end("Something went wrong. And we are reporting a custom error message.");
+    });
+    const httpsProxy = http_proxy_1.default.createServer({
+        target: "http://localhost:9092",
+        ssl: {
+            key: fs_1.default.readFileSync(Config_1.default.key, "utf8"),
+            cert: fs_1.default.readFileSync(Config_1.default.cert, "utf8")
+        }
+    });
+    httpsProxy.listen(9093);
+    httpsProxy.on("error", function (err, req, res) {
+        console.log(err);
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end("Something went wrong. And we are reporting a custom error message.");
+    });
     new skyserver_1.WebServer(Config_1.default, async () => {
     }, (req, res) => res.response({
         headers: {
--- src/Boot.ts
+++ src/Boot.ts
@@ -5,14 +5,34 @@ import Config from "./Config";
 
 export default async function boot() {
 
-    httpProxy.createServer({
+    const wssProxy = httpProxy.createServer({
         target: "ws://localhost:9090",
         ws: true,
         ssl: {
             key: fs.readFileSync(Config.key, "utf8"),
             cert: fs.readFileSync(Config.cert, "utf8")
         }
-    }).listen(9091);
+    });
+    wssProxy.listen(9091);
+    wssProxy.on("error", function (err, req, res) {
+        console.log(err);
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end("Something went wrong. And we are reporting a custom error message.");
+    });
+
+    const httpsProxy = httpProxy.createServer({
+        target: "http://localhost:9092",
+        ssl: {
+            key: fs.readFileSync(Config.key, "utf8"),
+            cert: fs.readFileSync(Config.cert, "utf8")
+        }
+    });
+    httpsProxy.listen(9093);
+    httpsProxy.on("error", function (err, req, res) {
+        console.log(err);
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end("Something went wrong. And we are reporting a custom error message.");
+    });
 
     new WebServer(Config, async () => {
         // ignore.
